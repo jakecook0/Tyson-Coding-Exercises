750a88127c6168eb52566312b8a317b7afc4b333
Jake Cook <jake.w.cook@sdl.usu.edu>
2022-01-27 15:07:37 -0700

    add user input exercises

---
+++ 01-basics_math_variables_input/inputs.py
@@ -0,0 +1,11 @@
+# Exercises to help better understand user input and value conversion
+
+# Get a sentence from the user and print it back out
+
+
+# Ask the user for a number, add it to another number and print it to the terminal
+#       hint: remember to convert the number you get from the user to an Integer
+
+
+# Ask the user for a word. Add the word to the end of the sentence "You gave me a: " and print out the combined sentence
+base_sentence = "You gave me a: "
---
+++ 01-basics_math_variables_input/math.py
@@ -0,0 +1,21 @@
+# Complete the following items demonstrating basic, common math
+
+# 1) Add any two or more numbers and print them to the terminal
+
+
+# 2) Multiply any two numbers and print them to the terminal
+
+
+# 3) Divide any two numbers and print them to the terminal
+
+
+# 4) Subtract two numbers, one of which is received from user input
+
+
+# 5) Get two numbers from the user and return the result of:
+#       a. adding them together
+#       b. dividing them together
+
+
+# 6) Figure out how to do the calculation 2^3 (or "2 to the power 3")
+#       HINT: You use a symbol but not the carat (^) character; 
